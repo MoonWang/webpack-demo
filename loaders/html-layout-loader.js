be3ea527877e517b4917d605346d516806c1f8cd
阿柴 <wwyg1213@163.com>
2019-03-20 21:05:37 +0800

    html-layout-loader base2

--- loaders/html-layout-loader.js
+++ loaders/html-layout-loader.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const fs = require('fs');
 const loaderUtils = require('loader-utils');
 const defaultOptions = {
-    placeholder: '{{__content__}}'
-}
+    placeholder: '{{__content__}}',
+    decorator: 'layout'
+};
 
 module.exports = function (source) {
     // 需要是异步 loader
@@ -10,11 +12,25 @@ module.exports = function (source) {
     // 合并默认参数和用户输入参数
     const options = Object.assign({}, defaultOptions, loaderUtils.getOptions(this));
     // 可以用 schema-utils 验证下是否设定了 layout 参数，此处略过
-    const { placeholder, layout } = options;
+    const { placeholder, layout, decorator } = options;
 
-    fs.readFile(layout, 'utf8', (err, html) => {
-        html = html.replace(placeholder, source);
-        // 最后一个 loader 输出需要是可执行的 js 代码，不能直接输出 html
-        callback(null, `module.exports = ${JSON.stringify(html)}`);
-    })
+    // 验证 html 文件中是否存在执行的语句，来声明模板文件
+    let reg1 = new RegExp(`@${decorator}\\((.+)\\)`);
+    let match1 = source.match(reg1);
+    if (match1) {
+        // 相对路径转换成绝对路径
+        fs.readFile(path.join(this.context, match1[1]), 'utf8', (err, html) => {
+            // 源文件先去掉声明语句
+            source = source.replace(match1[0], '');
+            html = html.replace(placeholder, source);
+            // 最后一个 loader 输出需要是可执行的 js 代码，不能直接输出 html
+            callback(null, `module.exports = ${JSON.stringify(html)}`);
+        });
+    } else {
+        fs.readFile(layout, 'utf8', (err, html) => {
+            html = html.replace(placeholder, source);
+            // 最后一个 loader 输出需要是可执行的 js 代码，不能直接输出 html
+            callback(null, `module.exports = ${JSON.stringify(html)}`);
+        })
+    }
 };
